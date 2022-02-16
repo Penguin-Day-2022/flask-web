7e5150c71daaaef7fb759f9193a63b89fde84603
Giranzo <99503924+Giranzo@users.noreply.github.com>
2022-02-16 14:27:45 -0300

    Actualizacion DHT11 + Web

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
 from flask import Flask, render_template
-
+import Adafruit_DHT
 app = Flask(__name__)
 
 
 @app.route("/")
 @app.route("/index")
 def index():
+	 
 	return render_template("index.html")
 
 @app.route("/proyecto")
 def proyecto():
-	return render_template("proyecto.html")
+	sensor = Adafruit_DHT.DHT11
+	pin = 23
+	humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+	templateData = {
+		"humedad" : humidity,
+		"temperatura" : temperature
+	}
+	return render_template("proyecto.html", **templateData)
 
 @app.route("/faqs")
 def faqs():
